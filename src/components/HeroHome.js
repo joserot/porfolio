8f5ab0ff66daec7e26ab2efd354c541b52be6670
joserot <jose.rotchen14@gmail.com>
2022-08-25 11:31:07 -0300

    add profile photo

--- src/components/HeroHome.js
+++ src/components/HeroHome.js
@@ -6,7 +6,7 @@ import Presentation from "./Presentation";
 const HeroHome = () => {
 	return (
 		<section className="container grid grid-cols-2 grid-rows-2 gap-2 items-start justify-center justify-items-center content-center mt-5  xl:w-4/5">
-			<div className="col-start-1 row-start-1 sm:row-start-1 sm:row-end-3s max-h-fit ">
+			<div className="col-start-1 row-start-1 sm:row-start-1 sm:row-end-3s max-h-fit w-full flex justify-center sm:justify-end">
 				<Photo />
 			</div>
 			<div className="sm:col-start-2 sm:row-start-2 flex flex-wrap gap-1 sm:h-10 max-h-fit">
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Image from "next/image";
+import photo from "../public/photo.jpeg";
 
 const Photo = () => {
 	return (
 		<div className="bg-white h-36 w-36 sm:h-40 sm:w-40 md:h-48 md:w-48 rounded shadow-2xl mx-1">
-			FOTO
+			<Image
+				src={photo}
+				objectFit="contain"
+				layout="responsive"
+				radioGroup="5"
+				className="rounded"
+			/>
 		</div>
 	);
 };
